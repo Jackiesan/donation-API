f1f68d730cbee49e715f05e5cf1cae8f7547cfde
Jackie <jonofre16@hotmail.com>
2018-07-09 21:08:42 -0700

    Added index and show to organizations controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,11 @@
 class OrganizationsController < ApplicationController
+  def index
+    organizations = Organization.all;
+    render json: {status: 'SUCCESS', message: 'Loaded organizations', data: organizations }, status: :ok
+  end
+
+  def show
+    organization = Organization.find(params[:id])
+    render json: {status: 'SUCCESS', message: 'Loaded organization', data: organization }, status: :ok
+  end
 end
