bcedeef9b8e859b6a179f2dd5b9be73394f3513f
Jackie <jonofre16@hotmail.com>
2018-07-13 16:56:50 -0700

    Added org wrapper to return details about queried organization

---
+++ lib/org_wrapper.rb
@@ -0,0 +1,36 @@
+class OrgWrapper
+  BASE_URL = "http://data.orghunter.com/v1/charitysearch"
+  KEY = ENV["API_KEY"]
+
+  def self.fetch(ein)
+    url = BASE_URL + "?user_key=" + KEY + "&ein=" + ein
+    response = HTTParty.get(url)
+    if response["code"] == 404
+      return []
+    else
+      db_org = Organization.find_by(ein:ein)
+      organization = self.construct_org(response["data"], db_org)
+      return organization
+    end
+  end
+
+  private
+
+  def self.construct_org(api_result, db_result)
+    DisplayOrg.new(
+      ein: api_result["ein"],
+      charityName: api_result["charityName"],
+      url: api_result["url"],
+      donationUrl: api_result["donationUrl"],
+      city: api_result["city"],
+      state: api_result["state"],
+      zipCode: api_result["zipCode"],
+      website: api_result["website"],
+      missionStatement: api_result["missionStatement"],
+      longitude: api_result["longitude"],
+      latitude: api_result["latitude"],
+      amazonWishlist: db_result["amazonWishlist"],
+      accepted_categories: db_result["accepted_categories"]
+    )
+  end
+end
